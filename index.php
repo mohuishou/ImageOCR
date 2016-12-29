31bfd10eb016fefcdfaf8175f7a99dd60c333088
mohuishou <1@lailin.xyz>
2016-12-29 14:20:10 +0800

    fixed test img url in index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'vendor/autoload.php';
-$image=new \Mohuishou\ImageOCR\Image("http://www.169ol.com/Mall/Code/getCode&1462104790492");
+$image=new \Mohuishou\ImageOCR\Image("http://www.169ol.com/Stream/Code/getCode");
 imagepng($image->_in_img,"./img/inImgTemp.png");
 //$image=new \ImageOCR\Image("./img/inImgTemp.png");
 $a=$image->find();
